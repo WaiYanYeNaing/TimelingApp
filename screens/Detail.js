31c7e58d786f2e0cbbd918b5f2dbffc283746aea
Kyi97 <kyisinthant199765@gmail.com>
2020-09-09 20:58:53 +0630

    edit

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,42 +1,296 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/CButton";
 import Container from "../components/Container";
 import Row from "../components/Row";
-import Header from "../components/Header";
 import { Icon } from "native-base";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { c1, c2, c3, c5, c4 } from "../themes/Colors";
-import TextM from "../components/TextM";
-import {
-  View,
-  StyleSheet,
-  Dimensions,
-  AsyncStorage,
-  Image,
-} from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import { c1, c2, c3, c5, c6 } from "../themes/Colors";
+import Text from "../components/TextR";
+import DatePicker from "react-native-datepicker";
+// import DropDownPicker from "react-native-dropdown-picker";
+import { View, StyleSheet, Picker, Image } from "react-native";
+import moment from "moment";
 
 export default function Detail({ navigation }) {
+  const Language = ["English", "Burmese", "Thai", "Chinese"];
+  const gender = ["Male", "Female", "Any"];
+  const Topics = [
+    "Language",
+    "Movies",
+    "Pets",
+    "Nature",
+    "Adventure",
+    "Climate",
+    "Handicaft",
+    "Writing",
+    "Beauty",
+    "Makeup",
+    "Fitness",
+    "Cosplay",
+    "Dancing",
+    "Singing",
+    "Anime",
+    "Art",
+    "Cars",
+    "Life",
+    "Fashion",
+    "Astrology",
+    "Music",
+    "Design",
+    "Technology",
+    "Finance",
+    "Travel",
+    "Health",
+    "Art",
+  ];
+  const [Gen, setGen] = useState("Male");
+  const [SelectTopic, setTopic] = useState([]);
+  const [date, setDate] = useState({ date: moment(new Date(), "DD-MM-YYYY") });
+  const [selected, setSelectedValue] = useState("English");
+
+  const pressHandler = (i) =>
+    SelectTopic.includes(i)
+      ? setTopic(SelectTopic.filter((s) => s !== i))
+      : setTopic([...SelectTopic, i]);
+
   return (
     <Container style={styles.container}>
-      {/* Header Action btn */}
-      {/* <Header style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon
-            type="Foundation"
-            name="arrow-left"
-            style={{ color: c3, fontSize: 30 }}
-          />
-        </TouchableOpacity>
-      </Header> */}
-      <View style={styles.Container}>
-
-      
+      {/* Profile Pic */}
+      <View style={styles.MainContainer}>
+        <Image
+          source={{
+            uri:
+              "https://static.turbosquid.com/Preview/2019/01/11__00_35_27/FM_TS2.pngEBF2CE9A-EAC1-48D4-BA04-987494C205A2Zoom.jpg",
+          }}
+          style={styles.propic}
+        />
+      </View>
+      {/* Gender */}
+      <View style={styles.Gender}>
+        <Text size={15}>Gender</Text>
+        <Row style={{ flexWrap: "wrap" }}>
+          {gender.map((v, i) => {
+            return (
+              <TouchableOpacity
+                activeOpacity={0.4}
+                key={i}
+                onPress={() => setGen(v)}
+              >
+                <View
+                  style={[
+                    styles.item,
+                    { backgroundColor: v == Gen ? c5 : "transparent" },
+                  ]}
+                >
+                  <Text color={v == Gen ? c1 : c6}>{v}</Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </Row>
+      </View>
+      {/* Birthday */}
+      <View style={styles.dob}>
+        <Text size={15}>Birthday</Text>
+        <DatePicker
+          style={styles.datepicker}
+          date={date.date}
+          mode="date"
+          placeholder="select date"
+          format="DD-MM-YYYY"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          iconComponent={
+            <Icon
+              size={30}
+              style={styles.dicon}
+              type="FontAwesome"
+              name="calendar"
+            />
+          }
+          customStyles={{
+            dateText: { color: c6 },
+            dateInput: {
+              marginLeft: 36,
+              borderWidth: 1,
+              borderColor: c5,
+              borderRadius: 10,
+            },
+          }}
+          onDateChange={(date) => {
+            setDate({ date: date });
+          }}
+        />
+      </View>
+      {/* Language */}
+      <View style={styles.Language}>
+        <Text size={15}>Language</Text>
+        <View style={styles.viewpicker}>
+          <Picker
+            style={styles.picker}
+            mode="dropdown"
+            selectedValue={selected}
+            onValueChange={(itemValue, i) => setSelectedValue(itemValue)}
+          >
+            {Language.map((item, i) => {
+              return <Picker.Item label={item} value={i} key={i} />;
+            })}
+          </Picker>
+        </View>
+      </View>
+      {/* Topic */}
+      <ScrollView>
+        <View style={styles.Topic}>
+          <Text size={15}>Pick some topics</Text>
+          <Row style={{ flexWrap: "wrap" }}>
+            {Topics.map((v, i) => {
+              return (
+                <TouchableOpacity
+                  activeOpacity={0.4}
+                  key={i}
+                  onPress={() => pressHandler(i)}
+                >
+                  <View
+                    style={[
+                      styles.Topicitem,
+                      {
+                        backgroundColor: SelectTopic.includes(i)
+                          ? c5
+                          : "transparent",
+                      },
+                    ]}
+                  >
+                    <Text color={SelectTopic.includes(i) ? c1 : c6}>{v}</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </Row>
+        </View>
+      </ScrollView>
+      {/* Btn Next */}
+      <View style={styles.BtnNext}>
+        <Button
+          backgroundColor={c3}
+          text={"Next"}
+          size={15}
+          width={340}
+          backgroundColor={c5}
+          onPress={() => navigation.navigate("Register")}
+        />
       </View>
     </Container>
   );
 }
 const styles = StyleSheet.create({
- container: {
-    backgroundColor: c1,
+  container: {
+    backgroundColor: c2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  MainContainer: {
+    margin: 5,
+    marginHorizontal: 20,
+    paddingTop: 5,
+  },
+
+  propic: {
+    width: 150,
+    height: 150,
+    borderRadius: 150 / 2,
+  },
+
+  picker: {
+    width: 350,
+    height: 37,
+    color: c6,
+  },
+  viewpicker: {
+    width: 360,
+    marginTop: 7,
+    marginBottom: 5,
+    borderWidth: 1,
+    borderColor: c5,
+    borderRadius: 10,
+  },
+
+  Language: {
+    marginHorizontal: 20,
+    paddingBottom: 10,
+    paddingTop: 4,
+    borderBottomWidth: 1,
+    borderColor: c5,
+  },
+
+  datepicker: {
+    marginTop: 10,
+    width: 360,
+    marginBottom: 5,
+    color: c6,
+  },
+
+  dicon: {
+    position: "absolute",
+    left: 0,
+    top: 4,
+    marginLeft: 0,
+    color: c5,
+  },
+
+  dob: {
+    marginHorizontal: 20,
+    paddingBottom: 10,
+    paddingTop: 4,
+  },
+
+  Gender: {
+    marginHorizontal: 20,
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+
+  Topic: {
+    marginHorizontal: 20,
+    paddingBottom: 10,
+    paddingTop: 10,
+    marginTop: 7,
+    marginBottom: 10,
+  },
+
+  item: {
+    paddingHorizontal: 17,
+    borderRadius: 0,
+    height: 40,
+    borderWidth: 1,
+    borderColor: c5,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 0,
+    marginVertical: 10,
+    width: 120,
+  },
+
+  Topicitem: {
+    paddingHorizontal: 12,
+    borderRadius: 10,
+    height: 40,
+    borderWidth: 1,
+    borderColor: c5,
+    justifyContent: "center",
+    alignItems: "center",
+    //marginRight: 10,
+    marginVertical: 10,
+    width: 113,
+    marginLeft: 8,
+  },
+
+  BtnNext: {
+    paddingTop: 20,
+    paddingHorizontal: 10,
+    height: 80,
+    borderTopStartRadius: 30,
+    borderTopEndRadius: 30,
+    alignItems: "center",
   },
 });
--- themes/Colors.js
+++ themes/Colors.js
@@ -3,3 +3,4 @@ export const c2 = "#283673";
 export const c3 = "#eeeef3";
 export const c4 = "#868b9d";
 export const c5 = "#fbde38";
+export const c6 = "#fff";
