b7e0b5d22f77a176f9ffc868651d5aff6f08267f
Kyi97 <kyisinthant199765@gmail.com>
2020-11-14 20:30:41 +0630

    edit

--- screens/Setting.js
+++ screens/Setting.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import { Icon } from "native-base";
 import React, { useEffect, useState } from "react";
+import { ListItem } from "react-native-elements";
 import { Image, StyleSheet, View } from "react-native";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import Container from "../components/Container";
@@ -11,77 +12,88 @@ import Text from "../components/TextR";
 import { c2, c3, c4, c5, c6 } from "../themes/Colors";
 
 export default function Settings({ navigation }) {
+  const list = [
+    {
+      title: "Notification",
+      icon: "notifications",
+    },
+    {
+      title: "Privacy",
+      icon: "lock",
+    },
+    {
+      title: "Security",
+      icon: "md-albums",
+    },
+    {
+      title: "Ads",
+      icon: "md-megaphone",
+    },
+    {
+      title: "Account",
+      icon: "md-person",
+    },
+    {
+      title: "Help",
+      icon: "md-help-circle",
+    },
+    {
+      title: "About",
+      icon: "md-information-circle",
+    },
+    {
+      title: "Theme",
+      icon: "md-moon",
+    },
+  ];
   return (
     <Container>
       {/* Action Header */}
       <Header style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon
-            type="Foundation"
-            name="arrow-left"
-            style={{ color: c3, fontSize: 30 }}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
+        <Row style={styles.row}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Icon
+              type="Foundation"
+              name="arrow-left"
+              style={{ color: c3, fontSize: 30, marginTop: -3 }}
+            />
+          </TouchableOpacity>
           <Text size={16} color={c6} style={styles.txt}>
-            Setting
+            Settings
           </Text>
-        </TouchableOpacity>
+        </Row>
       </Header>
 
-      {/* Title Icon */}
-      {/* <View style={styles.title_icon_text_container}>
-        <View style={styles.title_icon_container}>
-          <Image
-            source={require("../assets/images/connection.png")}
-            style={styles.title_icon}
-          />
-        </View>
-        <TextM size={20} style={styles.title_text}>
-          Setting
-        </TextM>
-      </View> */}
-      <ScrollView></ScrollView>
+      {/* My Account */}
+      <View style={{ flex: 1 }}>
+        {list.map((item, i) => (
+          <ListItem
+            key={i}
+            containerStyle={{ backgroundColor: c2 }}
+            bottomDivider
+          >
+            <Icon name={item.icon} style={{ color: c6 }} />
+            <ListItem.Content>
+              <ListItem.Title style={{ color: c6 }}>
+                {item.title}
+              </ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron />
+          </ListItem>
+        ))}
+      </View>
     </Container>
   );
 }
 
 const styles = StyleSheet.create({
   txt: {
-    marginTop: 3,
-  },
-  title_icon_text_container: {
-    marginVertical: 20,
+    textAlign: "left",
+    marginLeft: 50,
     alignItems: "center",
   },
-  title_icon_container: {
-    width: 70,
-    height: 70,
-    paddingTop: 3,
-    borderColor: "black",
-    borderWidth: 2,
-    backgroundColor: c5,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 100,
-  },
-  title_icon: {
-    width: 45,
-    height: 45,
-  },
-  title_text: {
-    marginTop: 10,
-  },
-  // ??????????????????????????????????????????????????????
-  friend_card: {
-    backgroundColor: c2,
-    height: 80,
-    paddingHorizontal: 15,
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginTop: 3,
-  },
-  thumbnail: {
-    marginRight: 15,
+  row: {
+    marginLeft: -4,
+    height: 38,
   },
 });
